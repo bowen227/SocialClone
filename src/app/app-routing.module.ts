2c616c3dfb97719676fddd8114c5cce9a4363fc3
bowen227 <owen.brent@gmail.com>
2020-11-11 14:30:57 -0500

    Added new route for details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './shared/auth.guard'
@@ -7,7 +8,8 @@ import { AuthGuard } from './shared/auth.guard'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'details', component: DetailsComponent }
 ];
 
 @NgModule({
