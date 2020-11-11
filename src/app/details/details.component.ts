1215df776c84b24bcd57a1113f4dece86c637b17
bowen227 <owen.brent@gmail.com>
2020-11-11 15:03:22 -0500

    Adding services

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendService } from '../shared/friend.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit(): void {
+  }
+
+  friendDetails(email: string) {
+    
   }
 
 }
